41a53a1bd7b6f67e52197ce63fec8d2c820ac6ee
Delio Labaddan <149133144+DellPogie@users.noreply.github.com>
2023-12-02 14:27:43 +0800

    Add files via upload

---
+++ Kotlin-Hello_World.kt
@@ -0,0 +1,9 @@
+fun main(args : Array<String>) {
+    println("ooooo   ooooo           oooo  oooo                   oooooo   oooooo     oooo                    oooo        .o8  .o. ")
+	println("`888'   `888'           `888  `888                    `888.    `888.     .8'                     `888        888  888 ")
+	println(" 888     888   .ooooo.   888   888   .ooooo.           `888.   .8888.   .8'    .ooooo.  oooo d8b  888   .oooo888  888 ")
+	println(" 888ooooo888  d88' `88b  888   888  d88' `88b           `888  .8'`888. .8'    d88' `88b `8888P    888  d88' `888  Y8P ")
+	println(" 888     888  888ooo888  888   888  888   888            `888.8'  `888.8'     888   888  888      888  888   888  `8' ")
+	println(" 888     888  888    .o  888   888  888   888 .o.         `888'    `888'      888   888  888      888  888   888  .o. ")
+	println("o888o   o888o `Y8bod8P' o888o o888o `Y8bod8P' Y8P          `8'      `8'       `Y8bod8P' d888b    o888o `Y8bod88P  Y8P ")
+}
